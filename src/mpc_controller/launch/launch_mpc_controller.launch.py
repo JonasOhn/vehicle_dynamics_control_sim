11bad552c600ab70d449e3057c911a8a824eab21
Jonas Ohnemus <ohnemujo@gmail.com>
2023-12-08 21:03:23 +0100

    changed mpc controller launch file to set DEBUG log leve

--- src/mpc_controller/launch/launch_mpc_controller.launch.py
+++ src/mpc_controller/launch/launch_mpc_controller.launch.py
@@ -3,11 +3,17 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "log_level",
+            default_value=["debug"],
+            description="Logging level",
+        ),
         Node(
             package='mpc_controller',
             executable='mpc_controller_node',
@@ -16,5 +22,6 @@ def generate_launch_description():
                 get_package_share_directory('mpc_controller'),
                 'config', 'mpc_controller.yaml')],
             output='log',
+            arguments=['--ros-args', '--log-level', 'debug'],
         ),
     ])
